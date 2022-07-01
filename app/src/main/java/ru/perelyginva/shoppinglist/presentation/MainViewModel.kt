9f58fb79441797eb43862f1e01c959f6715958cf
ViacheslavA <96613540+ViacheslavAP@users.noreply.github.com>
2022-07-01 09:59:48 +0200

    добавил в activity_main.xml разметку,  создал item_shop

--- app/src/main/java/ru/perelyginva/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/ru/perelyginva/shoppinglist/presentation/MainViewModel.kt
@@ -1,7 +1,6 @@
 package ru.perelyginva.shoppinglist.presentation
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+
 import androidx.lifecycle.ViewModel
 import ru.perelyginva.shoppinglist.data.ShopListRepositoryImpl
 import ru.perelyginva.shoppinglist.domain.DeleteShopUseCase
